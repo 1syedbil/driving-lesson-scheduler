9a6bede66d9472e0550b7234a44b4cac89eddc46
Bilal Syed <bsyed7633@conestogac.on.ca>
2024-10-22 00:30:21 -0400

    created the basic UI of the app and have the email functionality working. It now sends the email from the gmail account that I setup specifically for the app.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
 
 namespace drivingLessonScheduler
 {
@@ -23,6 +26,40 @@ namespace drivingLessonScheduler
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void submitLesson_Click(object sender, RoutedEventArgs e)
+        {
+            string emailSender = "continentalautomessage@gmail.com";
+            string emailReceiver = null;
+            string password = File.ReadAllText("gmail.txt");
+            if (instructor1.IsSelected)
+            {
+                emailReceiver = "1syedbil@gmail.com";
+            }
+
+            MailMessage lessonReminder = new MailMessage();
+            lessonReminder.To.Add(emailReceiver);
+            lessonReminder.From = new MailAddress(emailSender);
+            lessonReminder.Body = "Hello " + instructor1.Content.ToString() + "<br>You have a lesson with " + studNameInput.Text + " on, " + lessonDateSelection.SelectedDate.ToString() + ".";
+            lessonReminder.Subject = "New Lesson Scheduled";
+            lessonReminder.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+            smtpClient.EnableSsl = true;
+            smtpClient.Port = 587;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.Credentials = new NetworkCredential(emailSender, password);
+
+            try
+            {
+                smtpClient.Send(lessonReminder);
+                MessageBox.Show("Email has been sent.", "Success!", MessageBoxButton.OK);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
